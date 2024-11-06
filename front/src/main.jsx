eace5275d7d5b711e7c05ec72717a93e5cdf4502
Maxime ATTON <58865090+crawazaky@users.noreply.github.com>
2024-11-06 17:49:50 +0100

    feat: add react-router

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,12 +1,33 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+import App from './pages/App.jsx'
+import Login from './pages/Login.jsx'
+import SelectScenario from './pages/SelectScenario.jsx'
 import Header from './layout/Header.jsx'
+
+const router = createBrowserRouter([
+    {
+      path: "/", 
+      element: <App />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/selectScenario",
+      element: <SelectScenario />,
+    },
+  ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ front/src/pages/SelectScenario.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import '../App.css'
+
+function SelectScenario() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='flex justify-center'>
+        
+      </div>
+    </>
+  )
+}
+
+export default SelectScenario
