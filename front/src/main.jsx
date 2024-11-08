296aa21e8d2fc68254bc4d30d0afef7d20e02a32
Maxime ATTON <58865090+crawazaky@users.noreply.github.com>
2024-11-08 09:47:24 +0100

    feat: hide header in selected pages

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -3,8 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import {
     createBrowserRouter,
-    RouterProvider,
+    RouterProvider, 
+    useLocation,
+     Outlet
   } from "react-router-dom";
+
 import Start from './pages/Start.jsx'
 import Login from './pages/Login.jsx'
 import SelectScenario from './pages/SelectScenario.jsx'
@@ -16,53 +19,71 @@ import Game from './pages/Game.jsx'
 import Finish from './pages/Finish.jsx'
 import Header from './layout/Header.jsx'
 
-const router = createBrowserRouter([
+// Create a layout component to manage the header visibility
+const Layout = () => {
+    const location = useLocation();
+    
+    // Define routes where header should be hidden
+    const routesWithoutHeader = ['/game', '/emergency-bag', '/finish'];
+    
+    const shouldShowHeader = !routesWithoutHeader.includes(location.pathname);
+    
+    return (
+      <>
+        {shouldShowHeader && <Header />}
+        <Outlet />
+      </>
+    );
+  };
+  
+  
+  const router = createBrowserRouter([
     {
-      path: "/", 
-      element: <Start />,
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Start />,
+        },
+        {
+          path: "/login",
+          element: <Login />,
+        },
+        {
+          path: "/selectScenario",
+          element: <SelectScenario />,
+        },
+        {
+          path: "/emergency-bag",
+          element: <EmergencyBag />,
+        },
+        {
+          path: "/seisme",
+          element: <Seisme />,
+        },
+        {
+          path: "/inondation",
+          element: <Inondation />,
+        },
+        {
+          path: "/tsunami",
+          element: <Tsunami />,
+        },
+        {
+          path: "/game",
+          element: <Game />,
+        },
+        {
+          path: "/Finish",
+          element: <Finish />,
+        },
+      ],
     },
-    {
-      path: "/login",
-      element: <Login />,
-    },
-    {
-      path: "/selectScenario",
-      element: <SelectScenario />,
-    },
-    {
-      path: "/emergency-bag",
-      element: <EmergencyBag />,
-    },
-    {
-      path: "/seisme",
-      element: <Seisme />,
-    },
-
-    {
-      path: "/inondation",
-      element: <Inondation />,
-    },
-
-    {
-      path: "/tsunami",
-      element: <Tsunami />,
-    },
-
-    {
-      path: "/game",
-      element: <Game />,
-    },
-
-    {
-      path: "/Finish",
-      element: <Finish />,
-    },
-
   ]);
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    {/* <Header /> */}
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+  
+  // Update your root render
+  createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  );
