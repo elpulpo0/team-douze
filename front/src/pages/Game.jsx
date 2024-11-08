c4fa72519779b3075d378d46a8d38438a44177cf
elpulpo0 <chris.elpulpo@gmail.com>
2024-11-08 11:07:07 +0100

    update game design

--- front/src/pages/Game.jsx
+++ front/src/pages/Game.jsx
@@ -1,143 +1,151 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 import { useNavigate } from "react-router-dom";
-import '../App.css';
-import Scenario from "../scenarios/innondation.data";
+import '../App.css'
+import Scenario from "../scenarios/innondation.data"
 import Button_game from '../components/buttons/Button_game';
 
 function Game() {
     const navigate = useNavigate();
 
-    const [eventIndex, setEventIndex] = useState(0);
-    const [eventName, setEventName] = useState(null);
-    const [eventContext, setEventContext] = useState(null);
-    const [eventActions, setEventActions] = useState(null);
-    const [feedback, setFeedback] = useState(null);
-    const [background, setBackground] = useState(null);
-    const [score, setScore] = useState(0);  // Initialize score at 0
-    const [gold, setGold] = useState(0);
+const [eventIndex, setEventIndex] = useState(0)
+const [eventName, setEventName] = useState(null)
+const [eventContext, setEventContext] = useState(null)
+const [eventActions, setEventActions] = useState(null)
+const [feedback, setFeedback] = useState(null)
+const [background, setBackground] = useState(null)
+const [score, setScore] = useState(0)
+const [gold, setGold] = useState(0)
 
-    function sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+useEffect(() => {
+    loadEvent()
+}, [])
+
+useEffect(() => {
+    loadEvent()
+}, [eventIndex])
+
+useEffect(() => {
+    if(feedback) {
+        sleep(6000).then(() => { 
+            if(Scenario.evenements.length-1 > eventIndex) {
+                setEventIndex(eventIndex+1) 
+            } else {
+                console.log({ score: score, gold: gold })
+                navigate("/emergency-bag", { state: { score: score, gold: gold } });
+            }
+    
+         });
     }
 
-    useEffect(() => {
-        loadEvent();
-    }, []);
+}, [feedback])
 
-    useEffect(() => {
-        loadEvent();
-    }, [eventIndex]);
+const loadEvent = () => {
+    setFeedback(null)
+    setBackground(Scenario.evenements[eventIndex].image)
+    setEventName(Scenario.evenements[eventIndex].text)
+    setEventContext(Scenario.evenements[eventIndex].context)
+    setEventActions(Scenario.evenements[eventIndex].actions)
+}
 
-    useEffect(() => {
-        if (feedback) {
-            sleep(3000).then(() => { 
-                if (Scenario.evenements.length - 1 > eventIndex) {
-                    setEventIndex(eventIndex + 1);
-                } else {
-                    console.log({ score: score, gold: gold });
-                    navigate("/emergency-bag", { state: { score: score, gold: gold } });
-                }
-            });
-        }
-    }, [feedback]);
+const loadFeedback = (is_success) => {
+    if(is_success) {
+        setFeedback(Scenario.evenements[eventIndex].feedback)
+        updateScore(Scenario.evenements[eventIndex].avancement.succes.value)
+        updateGold(Scenario.evenements[eventIndex].avancement.succes.gold)
+    } else {
+        setFeedback(Scenario.evenements[eventIndex].feedback)
+        updateScore(Scenario.evenements[eventIndex].avancement.echec.value)
+        updateGold(Scenario.evenements[eventIndex].avancement.echec.gold)
+    }
+}
 
-    const loadEvent = () => {
-        setFeedback(null);
-        setBackground(Scenario.evenements[eventIndex].image);
-        setEventName(Scenario.evenements[eventIndex].text);
-        setEventContext(Scenario.evenements[eventIndex].context);
-        setEventActions(Scenario.evenements[eventIndex].actions);
-    };
+const updateScore = (update) => {
+    setScore(score + update)
+}
 
-    const loadFeedback = (is_success) => {
-        const event = Scenario.evenements[eventIndex];
-        setFeedback(event.feedback);
+const updateGold = (update) => {
+    setGold(gold + update)
+}
 
-        if (is_success) {
-            // Apply points from the 'succes' field in the JSON
-            updateScore(event.avancement.succes.value);
-            updateGold(event.avancement.succes.gold);
-        } else {
-            // Apply points from the 'echec' field in the JSON
-            updateScore(event.avancement.echec.value);
-            updateGold(event.avancement.echec.gold);
-        }
-    };
+const ExecuteEventAction = (action) => {
+    loadFeedback(action.is_success)
+}
 
-    const updateScore = (update) => {
-        setScore(prevScore => prevScore + update);  // Adjust score by the update value
-    };
-
-    const updateGold = (update) => {
-        setGold(prevGold => prevGold + update);     // Adjust gold by the update value
-    };
-
-    const ExecuteEventAction = (action) => {
-        loadFeedback(action.is_succes);  // Pass action's is_success status to loadFeedback
-    };
-
-    return (
-        <>
-            <div className="flex items-center justify-center min-h-screen background-container">
-                <div>
-                    {/* Main Content Container with larger width and height */}
-                    <div
-                        className="relative grid grid-cols-6 grid-rows-8 gap-4 w-full max-w-6xl h-[90vh] bg-cover bg-center rounded-lg shadow-lg pb-8"
-                        style={{ backgroundImage: `url("/images/${background}")` }}
-                    >
-                        {/* Contexte de l'événement */}
-                        <div className="col-span-6 row-start-1 row-span-2 flex flex-col justify-center">
-                            {/* En-tête avec Mission et Score */}
-                            <div className="w-full mb-7 flex justify-between bg-opacity-80 bg-white rounded-lg p-4 text-gray-800 shadow-md" style={{ justifyContent: 'space-between' }}>
-                                <div className='flex-col text-start'>
-                                    <h2 className="font-bold text-lg">Mission: {Scenario.nom}</h2>
-                                    <p className="text-sm">{Scenario.description}</p>
-                                </div>
-                                <div className='text-end'>
-                                    <p className="text-sm">Score: {score}</p>
-                                    <p className="text-sm">{gold}🪙</p>
-                                </div>
-                            </div>
-                            <div className="bg-white bg-opacity-75 p-4 rounded-lg shadow-md text-black w-3/4 text-center">
-                                {eventContext && eventContext}
-                            </div>
-                        </div>
+return (
+  <>
+    <div className="custom_font flex flex-col items-center justify-between min-h-screen background-container">
       
-                        {/* Nom de l'événement */}
-                        <div className="col-span-3 row-start-3">
-                            <div className="bg-white bg-opacity-75 p-6 rounded-lg shadow-md text-black">
-                                <h3 className="font-bold">{eventName && eventName}</h3>
-                            </div>
-                        </div>
-      
-                        {/* Boutons d'actions */}
-                        <div className="col-span-6 row-start-8 mt-6 flex justify-center">
-                            {eventActions && (
-                                <div className="flex gap-4 flex-wrap justify-center items-center">
-                                    {eventActions.map((action, index) => (
-                                        <Button_game
-                                            label={action.label}
-                                            key={index}
-                                            className="text-white bg-blue-500 hover:bg-blue-600 rounded-md px-4 py-2"
-                                            onClick={() => ExecuteEventAction(action)}
-                                        />
-                                    ))}
-                                </div>
-                            )}
-                        </div>
-                    </div>
-      
-                    {/* Feedback de l'utilisateur */}
-                    {feedback && (
-                        <div className="absolute left-4 bottom-4 flex items-center space-x-2 p-4 bg-white bg-opacity-80 rounded-lg shadow-md">
-                            <img src="/images/pompier_valid.png" alt="Validation" className="w-16 h-16 object-contain" />
-                            <p className="text-black">{feedback}</p>
-                        </div>
-                    )}
-                </div>
+      {/* Titre de la mission, centré en haut */}
+      <div className="w-full max-w-3xl mb-4 flex justify-between bg-opacity-80 bg-white rounded-lg p-4 text-gray-800 shadow-md">
+        <div className="flex-2 text-left">
+          <h2 className="font-bold text-lg">Mission: {Scenario.nom}</h2>
+          <p className="text-md">{Scenario.description}</p>
+        </div>
+        <div className="flex-1 text-right">
+          <div className="font-semibold text-lg">Score: {score}</div>
+          <div className="font-semibold text-lg">Gold: {gold}</div>
+        </div>
+      </div>
+
+      {/* Conteneur principal du contenu */}
+      <div
+        className="relative grid grid-cols-6 grid-rows-8 w-full max-w-6xl h-[80vh] bg-cover bg-center rounded-lg shadow-lg p-4"
+        style={{ backgroundImage: `url("/images/${background}")` }}
+      >
+        
+        {/* Contexte de l'événement */}
+        <div className="col-span-6 row-start-1 flex justify-center">
+          <div className="bg-white bg-opacity-75 p-4 rounded-lg shadow-md text-black w-3/4 text-center">
+            {eventContext && eventContext}
+          </div>
+        </div>
+
+        {/* Bulle de pensée pour l'événement */}
+        <div className="col-span-3 row-start-3">
+          <div className="bulle-pensee">
+            <h3 className="font-bold">{eventName && eventName}</h3>
+            <div className="mini-bulle mini-bulle1"></div>
+            <div className="mini-bulle mini-bulle2"></div>
+            <div className="mini-bulle mini-bulle3"></div>
+          </div>
+        </div>
+
+        {/* Boutons d'actions, centrés en bas */}
+        <div className="col-span-6 row-start-8 mt-4 flex justify-center">
+          {eventActions && (
+            <div className="flex flex-wrap gap-12 justify-between items-center w-full">
+              {eventActions.map((action, index) => (
+                <Button_game
+                  label={action.label}
+                  key={index}
+                  className="text-white bg-blue-500 hover:bg-blue-600 rounded-md px-4 py-2 w-full sm:w-auto"
+                  onClick={() => ExecuteEventAction(action)}
+                />
+              ))}
             </div>
-        </>
-    );
+          )}
+        </div>
+      </div>
+
+      {/* Feedback utilisateur, qui passe au-dessus des boutons */}
+      {feedback && (
+       <div className="feedback absolute bottom-5 left-0 right-0 mx-auto px-8 flex items-center justify-center p-4 bg-white bg-opacity-90 rounded-t-lg shadow-md z-10">
+       <img src="/images/pompier_valid.png" alt="Validation" className="w-16 h-16 object-contain mr-4" />
+       <p className="text-black text-xl">{feedback}</p>
+     </div>
+     
+      )}
+    </div>
+  </>
+);
+
+      
+      
+
 }
 
 export default Game;
